7ba6b73de7f0e1a458d6d7340e315cb9894e6b28
David Osborne <krendil@localhost.localdomain>
2012-10-20 21:34:31 +1000

    Added Liam's gui code

---
+++ src/asidesktop/AsiDesktopEstimator.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asidesktop;
+
+/**
+ *
+ * @author Liam
+ */
+
+import asidesktop.client.*;
+
+public class AsiDesktopEstimator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        AsiDesktopGui desktopGui = new AsiDesktopGui();
+    }
+}
